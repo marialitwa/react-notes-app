12320d6dac704454fdfe05902214aba56b3d4859
Maria Litwa <contact@marialitwa.com>
2023-04-26 16:22:37 +0200

    Make notes dynamic with props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const [notes, SetNotes] = useState([
+    {
+    id: nanoid(),
+    text: "This is the first note",
+    date: "20.04.2023",
+    },
+    {
+    id: nanoid(),
+    text: "This is the second note",
+    date: "22.04.2023",
+      },
+    {
+    id: nanoid(),
+    text: "This is the third note",
+    date: "24.04.2023",
+    }, 
+    {
+    id: nanoid(),
+    text: "This is a new note",
+    date: "26.04.2023",
+      }   
+  ]);
+
   return <div className="container">
     
-      <NotesList />
+      <NotesList notes={notes}/>
 
     </div>; 
 }
